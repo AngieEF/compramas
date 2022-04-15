97aaa032193329bf400a859795870c7a958096ce
AngieEF <80487348+AngieEF@users.noreply.github.com>
2022-04-14 21:58:02 -0500

    Add files via upload

---
+++ compramas/clientes.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>compras mas</title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/skeleton.css">
+        <link rel="stylesheet" href="css/style.css">
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" 
+crossorigin="anonymous">
+
+    </head>
+    <body>
+        <header class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2">
+                        <h1><img src="https://cdn-icons-png.flaticon.com/128/746/746900.png" width="40" align="left"></h1><h1 class="burger" align="left">Compra más</h1>
+                     </div>
+<!--<div class="col-md-8">-->
+  <div class="col-md-4">
+                    <ul class="menu">
+                      <li><a href="nosotros.html">NOSOTROS</a></li>
+                      <li><a href="index.html">PRODUCTOS</a></li>
+                      <li><a href="clientes.html">CLIENTES</a></li>
+                      
+                    </ul>
+                  </div>
+                
+
+                   
+                    <div class="col-md-2">
+                        <ul>
+                            <li class="submenu"><img src="https://cdn-icons-png.flaticon.com/128/2786/2786394.png"  width="30" align="right"><div id="carrito"><p class="vacio">CARRITO VACIO</p>
+                                <table id="lista-carrito" class="u-full-width "> 
+                                    <thead>
+                                        <tr>
+                                            <td>Imagen </td>
+                                            <td>Platillos</td>
+                                            <th>Precio</th>
+                                        </tr>
+                                    </thead>
+                                     <tbody>
+
+                                      </tbody>
+                                        
+                                </table>
+                                <br>
+                                
+                                <a href="#" id="vaciar-carrito" class="button u-full-width btn btn-warning">VACIAR CARRITO</a>  
+                            </div>
+                        </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </header>
